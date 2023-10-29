8d5ffd800c57e03e55f171d137c4a7cebf50fc79
AJ <akomolafe@tamu.edu>
2023-10-29 12:29:24 -0500

    not only vehicles...

--- taipySite.py
+++ taipySite.py
@@ -17,7 +17,7 @@ Upload a picture, wait for it to appear and your prediction will be made below.
 
 &emsp;
 
-<|{path}|file_selector|label=Upload picture|on_action=createPrediction|extensions=.png,.jpg|> 
+<|{path}|file_selector|label=Upload File|hover_text={prediction}|on_action=createPrediction|extensions=.png,.jpg|> 
 
 <|{path}|image|width={w}|height={h}|hover_text=Your Image!|> 
 
@@ -40,24 +40,27 @@ def binarizeArr(path):
     for row in range(height):
         for col in range(width):
             value = SIZ[row][col] #All R, G, and B values are the same color/value
-            if (value >= 128): #White
-                binarizeImage[row][col] = 1
-            else: #Black
+            if (value >= 128): #White maps to black
                 binarizeImage[row][col] = 0
+            else: #Black maps to white (What is drawn)
+                binarizeImage[row][col] = 1
 
     print("Binarized Array Created")
     return binarizeImage
 
 def createPrediction(state):
     taipy.gui.State.assign(state, "prediction", "Loading...")
+    taipy.gui.State.refresh(state, "prediction")
     BW = binarizeArr(state.path) #numpy array of 1s and 0s
 
     #Connect to ML here
     model = tf.keras.models.load_model("model.h5")
-    currPrediction = model.predict(np.array([BW]))
+    predictionArr = model.predict(np.array([BW]))
     fileNames=['weather.ndjson', 'instrument.ndjson', 'weapon.ndjson', 'plant.ndjson', 'writing_utensil.ndjson', 'construction.ndjson', 'cats.ndjson', 'tool.ndjson', 'accessory.ndjson', 'shape.ndjson', 'one_liner.ndjson', 'terrain.ndjson', 'sport.ndjson', 'fruit.ndjson', 'vehicle.ndjson']
-    result=fileNames[np.argmax(currPrediction[0])].replace('.ndjson','')
-    taipy.gui.State.assign(state, "prediction", result)
+    result=fileNames[np.argmax(predictionArr[0])].replace('.ndjson','')
+    print(result)
+    taipy.gui.State.assign(state, "prediction", str(result))
+    taipy.gui.State.refresh(state, "prediction")
     # except Exception as ex:
     #     print(type(ex).__name__, ex)
     #     taipy.gui.State.assign(state, "prediction", "Error! Ran out of memory?")

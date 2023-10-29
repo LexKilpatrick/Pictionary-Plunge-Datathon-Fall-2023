2fae75d4c2edee5ec846ac21f3e63d03376447c1
AJ <akomolafe@tamu.edu>
2023-10-29 03:52:04 -0500

    taipy including prediction

--- taipySite.py
+++ taipySite.py
@@ -1,25 +1,41 @@
 from taipy.gui import Gui
+import taipy
 import matlab.engine
 import matlab as ml
-import imageio.v3 as iio
 import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from engineio.async_drivers import gevent
 
 path = None
+prediction = "Waiting for input..."
+w = "160px"
+h = "160px"
 
 md = '''# Pictionary Plunge 
+Upload a picture, wait for it to appear and your prediction will be made below.
+
+&emsp;
+
 <|{path}|file_selector|label=Upload picture|on_action=createPrediction|extensions=.png,.jpg|> 
 
-<|{path}|image|on_action=clickCheck|width=128px|height=128px|>
+<|{path}|image|width={w}|height={h}|hover_text=Your Image!|> 
+
+Prediction: <|{prediction}|>.
 '''
 
 def binarizeArr(path):
     eng = matlab.engine.start_matlab()
     RGB = eng.imread(path)
     GRY = eng.rgb2gray(RGB)
-    targetSize = np.array([256.0, 256.0])
+    arrHeight = 500
+    arrWidth = 500
+    targetSize = np.array([float(arrHeight), float(arrWidth)])
     SIZ = eng.resize(GRY, targetSize)
+
     height = len(SIZ)
     width = len(SIZ[0])
+
     binarizeImage = np.ndarray(shape=(height, width))
     for row in range(height):
         for col in range(width):
@@ -28,12 +44,23 @@ def binarizeArr(path):
                 binarizeImage[row][col] = 1
             else: #Black
                 binarizeImage[row][col] = 0
+
+    print("Binarized Array Created")
     return binarizeImage
 
 def createPrediction(state):
-    global path
-    path = state.path
-    BW = binarizeArr(path) #numpy array of 1s and 0s
+    state.prediction = "Loading..."
+    BW = binarizeArr(state.path) #numpy array of 1s and 0s
+    problem = False
+
     #Connect to ML here
+    model = tf.keras.models.load_model("model.h5")
+    try:
+        state.prediction = model.predict(BW)
+    except:
+        problem = True
+    if (problem):
+        state.prediction = "Error! Ran out of memory?"
+    print("Create Prediction Done")
 
 Gui(md).run()

1e373aa1b9d882bdbe0b08128b78532151b1d1a8
AJ <akomolafe@tamu.edu>
2023-10-29 00:26:05 -0500

    taipy site with resize to 256

--- taipySite.py
+++ taipySite.py
@@ -16,12 +16,14 @@ def binarizeArr(path):
     eng = matlab.engine.start_matlab()
     RGB = eng.imread(path)
     GRY = eng.rgb2gray(RGB)
-    height = len(GRY)
-    width = len(GRY[0])
+    targetSize = np.array([256.0, 256.0])
+    SIZ = eng.resize(GRY, targetSize)
+    height = len(SIZ)
+    width = len(SIZ[0])
     binarizeImage = np.ndarray(shape=(height, width))
     for row in range(height):
         for col in range(width):
-            value = GRY[row][col] #All R, G, and B values are the same color/value
+            value = SIZ[row][col] #All R, G, and B values are the same color/value
             if (value >= 128): #White
                 binarizeImage[row][col] = 1
             else: #Black
